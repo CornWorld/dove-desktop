7026a20fe8173e4c9624b02188c99926ff9ebc44
CornWorld <cw@had.ink>
2024-10-06 08:53:16 +0800

    [Refactor] Move utility functions outside the function body

--- src/component/window.tsx
+++ src/component/window.tsx
@@ -1,6 +1,7 @@
 import {create} from "zustand/react";
 import {screenStore} from "../screen.tsx";
 import {createDragState, DraggableState} from "../utils/drag.ts";
+import {useLayoutEffect} from "react";
 
 interface WindowState {
     title: string;
@@ -22,11 +23,59 @@ const windowStore = create<WindowState>((set) => ({
     icon: '/assets/breeze-icons/icons/apps/16/anydesk.svg',
     widgetId: 'widget1',
     active: true,
-    height: 300, width: 200,
-    x: 500, y: 300,
+    height: 675, width: 931,
+    x: 80, y: 45,
     setPos: (x, y) => set({ x, y }),
     drag: createDragState(set),
 }));
+
+const rePosition = (x:number, y:number) => {
+    // check if the window is out of bounds
+    const state = windowStore.getState();
+    const screen = screenStore.getState().Screen;
+    if(x < 0) {
+        x = 0;
+    } else if(x + state.width > screen.width) {
+        x = screen.width - state.width;
+    }
+    if (y < 0) {
+        y = 0;
+    } else if (y + state.height > screen.height) {
+        y = screen.height - state.height;
+    }
+
+    state.setPos(x, y);
+}
+
+
+const onMouseMove = (e:MouseEvent) => {
+    const state = windowStore.getState();
+    if(state.drag.dragging) {
+        const newX = e.clientX - state.drag.offsetX;
+        const newY = e.clientY - state.drag.offsetY;
+
+        rePosition(newX, newY);
+    }
+};
+
+const onMouseUp = () => {
+    const s = windowStore.getState();
+    if (s.drag.dragging) {
+        window.removeEventListener('mousemove', onMouseMove);
+        window.removeEventListener('mouseup', onMouseUp);
+        s.drag.stopDrag();
+    }
+}
+
+const onMouseDown = (e:MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const state = windowStore.getState();
+    state.drag.startDrag(e.clientX - state.x, e.clientY - state.y);
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
+}
+
 
 export const Window = () => {
     const state = windowStore((s)=>s);
@@ -37,46 +86,10 @@ export const Window = () => {
         height: state.height+'px',
     };
 
-    const onMouseMove = (e:MouseEvent) => {
+    useLayoutEffect(() => {
         const state = windowStore.getState();
-        if(state.drag.dragging) {
-            let newX = e.clientX - state.drag.offsetX;
-            let newY = e.clientY - state.drag.offsetY;
-
-            // check if the window is out of bounds
-            const screen = screenStore.getState().Screen;
-            if(newX < 0) {
-                newX = 0;
-            } else if(newX + state.width > screen.width) {
-                newX = screen.width - state.width;
-            }
-            if (newY < 0) {
-                newY = 0;
-            } else if (newY + state.height > screen.height) {
-                newY = screen.height - state.height;
-            }
-
-            state.setPos(newX, newY);
-        }
-    };
-
-    const onMouseUp = () => {
-        const s = windowStore.getState();
-        if (s.drag.dragging) {
-            window.removeEventListener('mousemove', onMouseMove);
-            window.removeEventListener('mouseup', onMouseUp);
-            s.drag.stopDrag();
-        }
-    }
-
-    const onMouseDown = (e:MouseEvent) => {
-        e.preventDefault();
-        e.stopPropagation();
-        const state = windowStore.getState();
-        state.drag.startDrag(e.clientX - state.x, e.clientY - state.y);
-        window.addEventListener('mousemove', onMouseMove);
-        window.addEventListener('mouseup', onMouseUp);
-    }
+        rePosition(state.x, state.y);
+    }, []);
 
     return <div className={'window'} style={style}>
         <div className={'headerbar'} onMouseDown={(e)=>onMouseDown(e as unknown as MouseEvent)}>
--- src/workspace/index.tsx
+++ src/workspace/index.tsx
@@ -55,6 +55,113 @@ const IconWidth = 103;
 const IconHeight = 93;
 const IconMargin = 5;
 
+const iconGridSnap = (x: number, y: number) => {
+    return {
+        snappedX: Math.round(x / (IconWidth + IconMargin)) * (IconWidth + IconMargin),
+        snappedY: Math.round(y / (IconHeight + IconMargin)) * (IconHeight + IconMargin),
+    }
+}
+
+const iconSearchPlace = (x: number, y: number) => {
+    const screen = screenStore.getState().Screen;
+    const positions = [];
+
+    for (let i = 0; i < screen.width; i += IconWidth + IconMargin) {
+        for (let j = 0; j < screen.height - 44; j += IconHeight + IconMargin) {
+            if (getIconIndexByPos(i, j) === -1) {
+                positions.push({x: i, y: j});
+            }
+        }
+    }
+
+    positions.sort((a, b) => {
+        const distA = Math.hypot(a.x - x, a.y - y);
+        const distB = Math.hypot(b.x - x, b.y - y);
+        return distA - distB;
+    });
+
+    return positions.length > 0 ? positions[0] : null;
+}
+
+const getIconIndexByPos = (x: number, y: number) => {
+    let res = -1;
+    workspaceStore.getState().icons.map((icon, index) => {
+        if (icon.x === x && icon.y === y) res = index;
+    });
+    return res;
+}
+
+const onMouseMove = (e: MouseEvent) => {
+    const state = workspaceStore.getState();
+
+    if (state.drag.dragging) {
+        const index = state.drag.dragging - 1;
+        let newX = e.clientX - state.drag.offsetX;
+        let newY = e.clientY - state.drag.offsetY;
+
+        state.selectIcon(index, true);
+
+        // check if the icon is out of bounds
+        const screen = screenStore.getState().Screen;
+        if (newX < 0) {
+            newX = 0;
+        } else if (newX + IconWidth > screen.width) {
+            newX = screen.width - IconWidth;
+        }
+        if (newY < 0) {
+            newY = 0;
+        } else if (newY + IconHeight > screen.height - 44) {
+            newY = screen.height - IconHeight - 44;
+        }
+
+        state.setIconPos(index, newX, newY);
+    }
+};
+
+const onMouseUp = () => {
+    const state = workspaceStore.getState();
+    if (state.drag.dragging) {
+        const index = state.drag.dragging - 1;
+        let {x, y} = state.icons[index];
+        const {snappedX, snappedY} = iconGridSnap(x, y);
+
+        window.removeEventListener('mousemove', onMouseMove);
+        window.removeEventListener('mouseup', onMouseUp);
+
+        const resIndex = getIconIndexByPos(snappedX, snappedY);
+        // TODO
+
+        state.drag.stopDrag();
+
+        if (resIndex === -1) {
+            state.setIconPos(index, snappedX, snappedY);
+        } else {
+            const p = iconSearchPlace(x, y);
+            if (p !== null) state.setIconPos(index, p.x, p.y);
+            else throw new Error('No where could place icon'); // TODO
+        }
+    }
+}
+
+const onMouseDown = (e: MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    // get the icon index
+    let dataIndex = (e.target as HTMLElement).getAttribute('data-index');
+    if (dataIndex === null) {
+        workspaceStore.getState().cancelSelection();
+    } else {
+        const dragging = parseInt(dataIndex) + 1;
+        const state = workspaceStore.getState();
+
+        state.selectIcon(dragging - 1);
+        const {x, y} = state.icons[dragging - 1];
+        state.drag.startDrag(e.clientX - x, e.clientY - y, dragging);
+        window.addEventListener('mousemove', onMouseMove);
+        window.addEventListener('mouseup', onMouseUp);
+    }
+}
+
 export const Workspace = () => {
     const state = workspaceStore((s) => s);
     useEffect(() => {
@@ -72,113 +179,6 @@ export const Workspace = () => {
             state.setIconPos(i, newX, newY);
         }
     }, [])
-
-    const iconGridSnap = (x: number, y: number) => {
-        return {
-            snappedX: Math.round(x / (IconWidth + IconMargin)) * (IconWidth + IconMargin),
-            snappedY: Math.round(y / (IconHeight + IconMargin)) * (IconHeight + IconMargin),
-        }
-    }
-
-    const iconSearchPlace = (x: number, y: number) => {
-        const screen = screenStore.getState().Screen;
-        const positions = [];
-
-        for (let i = 0; i < screen.width; i += IconWidth + IconMargin) {
-            for (let j = 0; j < screen.height - 44; j += IconHeight + IconMargin) {
-                if (getIconIndexByPos(i, j) === -1) {
-                    positions.push({x: i, y: j});
-                }
-            }
-        }
-
-        positions.sort((a, b) => {
-            const distA = Math.hypot(a.x - x, a.y - y);
-            const distB = Math.hypot(b.x - x, b.y - y);
-            return distA - distB;
-        });
-
-        return positions.length > 0 ? positions[0] : null;
-    }
-
-    const getIconIndexByPos = (x: number, y: number) => {
-        let res = -1;
-        workspaceStore.getState().icons.map((icon, index) => {
-            if (icon.x === x && icon.y === y) res = index;
-        });
-        return res;
-    }
-
-    const onMouseMove = (e: MouseEvent) => {
-        const state = workspaceStore.getState();
-
-        if (state.drag.dragging) {
-            const index = state.drag.dragging - 1;
-            let newX = e.clientX - state.drag.offsetX;
-            let newY = e.clientY - state.drag.offsetY;
-
-            state.selectIcon(index, true);
-
-            // check if the icon is out of bounds
-            const screen = screenStore.getState().Screen;
-            if (newX < 0) {
-                newX = 0;
-            } else if (newX + IconWidth > screen.width) {
-                newX = screen.width - IconWidth;
-            }
-            if (newY < 0) {
-                newY = 0;
-            } else if (newY + IconHeight > screen.height - 44) {
-                newY = screen.height - IconHeight - 44;
-            }
-
-            state.setIconPos(index, newX, newY);
-        }
-    };
-
-    const onMouseUp = () => {
-        const s = workspaceStore.getState();
-        if (s.drag.dragging) {
-            const index = s.drag.dragging - 1;
-            let {x, y} = s.icons[index];
-            const {snappedX, snappedY} = iconGridSnap(x, y);
-
-            window.removeEventListener('mousemove', onMouseMove);
-            window.removeEventListener('mouseup', onMouseUp);
-
-            const resIndex = getIconIndexByPos(snappedX, snappedY);
-            // TODO
-
-            s.drag.stopDrag();
-
-            if (resIndex === -1) {
-                state.setIconPos(index, snappedX, snappedY);
-            } else {
-                const p = iconSearchPlace(x, y);
-                if (p !== null) state.setIconPos(index, p.x, p.y);
-                else throw new Error('No where could place icon'); // TODO
-            }
-        }
-    }
-
-    const onMouseDown = (e: MouseEvent) => {
-        e.preventDefault();
-        e.stopPropagation();
-        // get the icon index
-        let dataIndex = (e.target as HTMLElement).getAttribute('data-index');
-        if (dataIndex === null) {
-            workspaceStore.getState().cancelSelection();
-        } else {
-            const dragging = parseInt(dataIndex) + 1;
-            const state = workspaceStore.getState();
-
-            state.selectIcon(dragging - 1);
-            const {x, y} = state.icons[dragging - 1];
-            state.drag.startDrag(e.clientX - x, e.clientY - y, dragging);
-            window.addEventListener('mousemove', onMouseMove);
-            window.addEventListener('mouseup', onMouseUp);
-        }
-    }
 
     return <div css={{
         width: '100%',
